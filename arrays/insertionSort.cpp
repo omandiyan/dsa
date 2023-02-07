93a3399add0147279cf97cce632bb566d4b4f83a
om mandiyan <81075346+omandiyan@users.noreply.github.com>
2023-02-07 22:11:21 +0530

    Insertion sort using vectors

--- arrays/insertionSort.cpp
+++ arrays/insertionSort.cpp
@@ -1,26 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+vector<int> insertionSort(int n, vector<int> arr)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int v = 0;
+        for (int j = i - 1; i >= 0; j--)
+        {
+            v = j + 1;
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[v] = temp;
+    }
+    return arr;
+}
+
 int main(int argc, char const *argv[])
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    for(int i=1;i<n;i++){
-        int current=arr[i];
-        int j=i-1;
-        while(arr[j]>current){
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1]=current;
-    }
-      for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    vector<int> arr;
+    arr.push_back(5);
+    arr.push_back(4);
+    arr.push_back(3);
+    arr.push_back(2);
+    arr.push_back(1);
+    int n = 5;
+    arr = insertionSort(n, arr);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
     }
 
-    
     return 0;
 }
